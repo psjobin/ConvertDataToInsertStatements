4d567d9cd5c07479a9e0c4db0421ee1469cad046
psjobin <psjobin@gmail.com>
2021-06-13 18:21:30 -0500

    update

--- trying this thing with javascript/script.js
+++ trying this thing with javascript/script.js
@@ -3,8 +3,8 @@ function Process(){
     var jsonData = document.getElementById("taTableData").value; 
     var a = JSON.parse(JSON.stringify(JSON.parse( jsonData).headers))
     //console.log(a); 
-    var statement = ""
     for(key in a){
+        var statement = ""
         //console.log(key, '->' , a[key])
         statement += "insert into " + key + "("
         for(x in a[key]){
@@ -12,8 +12,21 @@ function Process(){
             statement += a[key][x] + ","
         }
         statement = statement.slice(0,-1)
-        statement += ");"
-        console.log(statement);
-    }
+        statement += ") values ("
 
+        var rows = JSON.parse( jsonData).rows
+       // console.log(rows[key])
+            for(key2 in rows[key]){
+                //console.log(rows[key][key2])
+                for(val in rows[key][key2]){
+                    //console.log(rows[key][key2][val])
+                    statement += "'" + rows[key][key2][val] + "',"
+                }
+                statement = statement.slice(0,-1)
+                statement += "),("
+            }
+            statement = statement.slice(0,-2)
+            statement += ";"
+            console.log(statement);
+    }
 }
