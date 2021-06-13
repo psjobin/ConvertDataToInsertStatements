37cb74ccd027b3e3a84a8c38dbe0b3bcd6bdd7b7
psjobin <psjobin@gmail.com>
2021-06-13 18:29:55 -0500

    removed unnecessary commented out code and cleaned it up a bit

--- trying this thing with javascript/script.js
+++ trying this thing with javascript/script.js
@@ -1,32 +1,31 @@
 
-function Process(){
-    var jsonData = document.getElementById("taTableData").value; 
-    var a = JSON.parse(JSON.stringify(JSON.parse( jsonData).headers))
-    //console.log(a); 
-    for(key in a){
-        var statement = ""
-        //console.log(key, '->' , a[key])
-        statement += "insert into " + key + "("
-        for(x in a[key]){
-            //console.log(a[key][x])
-            statement += a[key][x] + ","
-        }
-        statement = statement.slice(0,-1)
-        statement += ") values ("
-
-        var rows = JSON.parse( jsonData).rows
-       // console.log(rows[key])
-            for(key2 in rows[key]){
-                //console.log(rows[key][key2])
-                for(val in rows[key][key2]){
-                    //console.log(rows[key][key2][val])
+function Process() {
+    try {
+        var jsonData = document.getElementById("taTableData").value;
+        var a = JSON.parse(jsonData).headers
+        for (key in a) {
+            var statement = ""
+            statement += "insert into " + key + "("
+            for (x in a[key]) {
+                statement += a[key][x] + ","
+            }
+            statement = statement.slice(0, -1)
+            statement += ") values ("
+            var rows = JSON.parse(jsonData).rows
+            for (key2 in rows[key]) {
+                for (val in rows[key][key2]) {
                     statement += "'" + rows[key][key2][val] + "',"
                 }
-                statement = statement.slice(0,-1)
+                statement = statement.slice(0, -1)
                 statement += "),("
             }
-            statement = statement.slice(0,-2)
+            statement = statement.slice(0, -2)
             statement += ";"
             console.log(statement);
+        }
+    } catch (error) {
+        alert("something went wrong - make sure your json is good!")
     }
+
+
 }
